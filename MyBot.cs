a6286a23c73bea0570f042be3c6bed4e8d48fe92
andras204 <85683781+andras204@users.noreply.github.com>
2023-07-23 17:22:43 +0200

    Initial commit

---
+++ MyBot.cs
@@ -0,0 +1,16 @@
+﻿using ChessChallenge.API;
+
+public class MyBot : IChessBot
+{
+
+    // HyperAgressor
+    // move prioritization:
+    // checkmate > attacking king / around king > taking pices > moving closer to king > moving closer to enemy piecies
+    // optional goal(s): add defense of own king to priority list, check if the move 
+
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
+    }
+}
